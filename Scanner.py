34df4ffcb09d3bd36ab138f25be3b999bb69cdb1
BunForFun <benhawcroft14@gmail.com>
2020-05-19 13:13:05 +0100

    Created a basic bar code/qr code scanner that will be used as a front end data input for the database

---
+++ Scanner.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+import pyzbar.pyzbar as pyzbar
+
+
+
+def main():
+    cap = cv2.VideoCapture(0)
+
+    i = 1
+
+    while True:
+        _, frame = cap.read()
+
+        decodedObjects = pyzbar.decode(frame)
+        if i == 1:
+            for obj in decodedObjects:
+                print("Data", obj.data)
+                X = obj.data
+                print("This is x: ", X)
+                i = 2
+                break
+
+        cv2.imshow("Frame", frame)
+
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+
+
+main()
+
+
+
